0e88c54f325460390c2e0b4a9bdff2638fc9569b
Afify Adel <afifyadel25@gmail.com>
2021-03-22 16:57:46 +0200

    Add Slider

---
+++ Web/OnlineShopping/src/app/modules/shared/slider/slider.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from '../../../../../node_modules/jquery'
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss']
+})
+export class SliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
